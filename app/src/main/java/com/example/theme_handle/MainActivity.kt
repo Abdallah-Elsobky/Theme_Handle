0e757d328ff7f6b7f690f873cb602c92e325fa45
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2025-06-18 00:02:01 +0300

    finish themes logic

--- app/src/main/java/com/example/theme_handle/MainActivity.kt
+++ app/src/main/java/com/example/theme_handle/MainActivity.kt
@@ -14,6 +14,11 @@ import com.example.theme_handle.dummy_data.Generator
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    override fun onStart() {
+        super.onStart()
+        SettingsActivity.applySavedDarkMode(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
--- app/src/main/java/com/example/theme_handle/SettingsActivity.kt
+++ app/src/main/java/com/example/theme_handle/SettingsActivity.kt
@@ -1,22 +1,60 @@
 package com.example.theme_handle
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.theme_handle.databinding.ActivitySettingsBinding
+import androidx.core.content.edit
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
 
+    private val PREF_DARK_MODE = "pref_dark_mode"
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.darkModeSwitch.setOnClickListener {
-            Toast.makeText(this, "Theme Changed", Toast.LENGTH_SHORT).show()
+        sharedPreferences = getSharedPreferences("app_preference", MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean(PREF_DARK_MODE, false)
+
+        binding.darkModeSwitch.isChecked = isDarkMode
+
+        binding.darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            applyDarkMode(isChecked)
+            saveDarkModePref(isChecked)
+        }
+    }
+
+    private fun saveDarkModePref(isChecked: Boolean) {
+        sharedPreferences.edit { putBoolean(PREF_DARK_MODE, isChecked) }
+    }
+
+    private fun applyDarkMode(isChecked: Boolean) {
+        AppCompatDelegate.setDefaultNightMode(
+            if (isChecked)
+                AppCompatDelegate.MODE_NIGHT_YES
+            else
+                AppCompatDelegate.MODE_NIGHT_NO
+        )
+        recreate()
+    }
+
+    companion object {
+        fun applySavedDarkMode(context: Context) {
+            val sharedPreferences = context.getSharedPreferences("app_preference", MODE_PRIVATE)
+            val isDarkMode = sharedPreferences.getBoolean("pref_dark_mode", false)
+            AppCompatDelegate.setDefaultNightMode(
+                if (isDarkMode)
+                    AppCompatDelegate.MODE_NIGHT_YES
+                else
+                    AppCompatDelegate.MODE_NIGHT_NO
+            )
         }
     }
 }
